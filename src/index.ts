b42d63a8b9fca6d584185afc1c6bb6e747c61b9b
Geoff Neal <geoffrey_neal@hotmail.com>
2023-08-31 16:29:55 +0100

    gql server serving csv file content

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
+import path from "path";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// Utils
+import { readCSV } from "./utils/data.js";
+
 const typeDefs = `#graphql
-    type Query {
-        test: Boolean!
-    }
+  # The types of car available
+  type Product {
+    vin: String
+    colour: String
+    make: String
+    model: String
+    price: Int
+  }
+
+  # Customer details
+  type Customer {
+    email: String
+    forename: String
+    surname: String
+    contact_number: String
+    postcode: String
+  }
+
+  type Query {
+    products: [Product],
+    customers: [Customer],
+  }
 `;
+
 const resolvers = {
   Query: {
-    test: () => true,
+    products: () => readCSV(path.resolve(process.cwd(), "data/product.csv")),
+    customers: () => readCSV(path.resolve(process.cwd(), "data/customer.csv")),
   },
 };
 
