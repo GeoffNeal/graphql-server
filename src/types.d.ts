0b65db11a0ab22fa4eb7ea58bf83b343f8194dbb
Geoff Neal <geoffrey_neal@hotmail.com>
2023-09-01 14:29:22 +0100

    Added ts types for Product and Customer

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+type Product = {
+  vin: string;
+  colour: string;
+  make: string;
+  model: string;
+  price: number;
+};
+
+type Customer = {
+  email: string;
+  forename: string;
+  surname: string;
+  contact_number: string;
+  postcode: string;
+};
