2dab21086f2983dbf61acb5c1c7a140d2dea334d
Geoff Neal <geoffrey_neal@hotmail.com>
2023-09-06 00:53:11 +0100

    Changed number input to text

--- client/src/components/CreateProduct.tsx
+++ client/src/components/CreateProduct.tsx
@@ -82,7 +82,6 @@ export default function CreateProduct() {
                 name="price"
                 id="price"
                 value={formValues.price}
-                type="number"
               />
             </FormCol>
 
